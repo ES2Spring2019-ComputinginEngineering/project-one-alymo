4d0a5b089d7a2f96f14a30f067056e0b53977e26
Mo <mo.liu@tufts.edu>
2019-02-19 16:20:38 -0500

    Update microbit-pendulum.py

--- microbit-pendulum.py
+++ microbit-pendulum.py
@@ -4,11 +4,12 @@ print("Part I: Simulation")
 
 import numpy
 
-def update_pendulum(acc,pos,vel,time1,time2):
+def update_pendulum(ang_pos, ang_vel, ang_acc, atime1,time2):
     dt = time2-time1
-    posNext = pos+vel*dt+1/2*acc*dt
-    velNext = vel+acc*dt
-    return posNext,velNext
+    ang_vel_new = ang_vel+ang_acc*dt
+    ang_pos_new = ang_pos+ang_vel*dt
+    ang_acc_new = 9.8*(pi/2-ang_pos_new)
+    return ang_pos_new, ang_vel_new, ang_acc_new
 
 def print_pendulum(time,pos,vel):
     print("TIME:     ", time)
@@ -17,14 +18,14 @@ def print_pendulum(time,pos,vel):
 
 pos = [0]
 vel = [0]
-acc = [0,1,2,3,4,4,2,2,1,0,0,0,0,-1,-1,-2,-2,-2,-3,-4,-4]
-time = numpy.linspace(0,20,21)
+ang_acc = 9.8*(pi/2-ang_pos)
+time = numpy.linspace(0,20,201)
 print_pendulum(time[0],pos[0],vel[0])
 
 i = 1
 while i < len(time):
-    posNext, velNext = update_system(acc[i],pos[i-1],vel[i-1],time[i-1],time[i])
+    posNext, velNext = update_pendulum(acc[i],pos[i-1],vel[i-1],time[i-1],time[i])
     pos.append(posNext)
     vel.append(velNext)
-    print_system(time[i],pos[i],vel[i])
+    print_pendulum(time[i],pos[i],vel[i])
     i += 1
