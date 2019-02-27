cd3c59d83a209c02d2a7d0878b4a42e996c9c0be
Mo <mo.liu@tufts.edu>
2019-02-27 15:55:12 -0500

    Update microbit_data_collection.py

--- microbit_data_collection.py
+++ microbit_data_collection.py
@@ -1,6 +1,22 @@
-import microbit
+import microbit, math, os, random
 
-fout = open("pendulum_data.txt", "w+")
+#cwd = os.getcwd()
 
+filename = "pendulum_data" + str(random.randint(1,999)) + ".txt"
 
-fout.write(time + "\t" + accleration + "\n")
+while not microbit.button_a.is_pressed():
+    microbit.sleep(50)
+
+with open(filename, "w") as file:
+    start_time = (microbit.running_time())/1000
+    while not microbit.button_b.is_pressed():
+        acceleration_x = microbit.accelerometer.get_x()
+        acceleration_y = microbit.accelerometer.get_y()
+        acceleration_z = microbit.accelerometer.get_z()
+        microbit.display.show(microbit.Image.HAPPY)
+        time = (microbit.running_time())/1000 - start_time
+        values = str(acceleration_x) + "," + str(acceleration_y) + "," + str(acceleration_z) + "," + str(time) + "\n"
+        file.write(values)
+        microbit.sleep(50)
+        print(values)
+file.close()
