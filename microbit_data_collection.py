a85de3b31849a5f7172736734458de8821e26aef
Mo <mo.liu@tufts.edu>
2019-02-27 21:15:56 -0500

    change

--- microbit_data_collection.py
+++ microbit_data_collection.py
@@ -10,11 +10,11 @@ while not microbit.button_a.is_pressed():
 with open(filename, "w") as file:
     start_time = (microbit.running_time())/1000
     while not microbit.button_b.is_pressed():
-        acceleration_x = microbit.accelerometer.get_x()
-        acceleration_y = microbit.accelerometer.get_y()
+        acceleration_x = (microbit.accelerometer.get_x())/1000
+        acceleration_y = (microbit.accelerometer.get_y())/1000
         microbit.display.show(microbit.Image.HAPPY)
         time = (microbit.running_time())/1000 - start_time
-        values = str(acceleration_x) + "," + str(acceleration_y) + "," + str(acceleration_z) + "," + str(time) + "\n"
+        values = str(acceleration_x) + "," + str(acceleration_y) + "," + str(time) + "\n"
         file.write(values)
         microbit.sleep(50)
         print(values)
