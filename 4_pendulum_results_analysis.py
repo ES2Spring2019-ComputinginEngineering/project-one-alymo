cb5a77e560c8184e9d1eacfabd44ea98e359ff9d
Mo <mo.liu@tufts.edu>
2019-03-05 16:07:38 -0500

    s

--- 4_pendulum_results_analysis.py
+++ 4_pendulum_results_analysis.py
@@ -11,7 +11,7 @@ t = []
 g = 9.81
  
 #openning the data file and extracting the data:
-with open ("pendulum_data_23.5.txt", "r") as file:
+with open ("pendulum_data_4.75.txt", "r") as file:
     for line in file:
         line = line.rstrip().split(",")
         y_acceleration = float(line[1])
@@ -31,23 +31,22 @@ theta = np.arcsin(y_noisy_filt / g)
 theta_pks, _ = sig.find_peaks(theta)
 
 #plotting y acceleration against time:
-plt.subplot(2, 1, 1)
+plt.figure()
 plt.plot(t, y_noisy_filt,'g-', t[y_noisy_filt_pks], y_noisy_filt[y_noisy_filt_pks], 'b.')
 plt.xlabel('Time (seconds)')
 plt.ylabel('Y Acceleration (m/s^2)')
 plt.title('Y Acceleration vs Time Filtered')
 plt.grid()
+plt.show()
 
 #plotting theta against time:
-plt.subplot(2, 1, 2)
+plt.figure()
 plt.plot(t, theta,'b-', t[y_noisy_filt_pks], theta[y_noisy_filt_pks], 'g.')
 plt.title('Noisy Median Filtered')
 plt.xlabel('Time (seconds)')
 plt.ylabel('Theta(radians)')
 plt.title('Theta vs Time Filtered')
 plt.grid()
-
-plt.tight_layout()
 plt.show()
 
 #Determining period by averaging time differences
