374760e6b525d10609012caece449bb924163b22
Alyssa Attonito <alyssa.attonito@tufts.edu>
2019-03-05 17:00:13 -0500

    Update

--- 3_plotting_period_and_length.py
+++ 3_plotting_period_and_length.py
@@ -8,8 +8,9 @@ Created on Mon Mar  4 15:29:19 2019
 
 import matplotlib.pyplot as plt
 
+#Note: These Numbers plot simulation data; need to be modified to graph other
 Length_of_Pendulum= [0.12065, 0.2159, 0.3429, 0.4699, 0.5969] 
-Period_of_Pendulum= [1.000, 1.207,1.461, 1.683, 1.882] #last data point made up
+Period_of_Pendulum= [0.6448, 0.6708,0.6210, 0.5008, 0.7255]
 
 
 plt.plot(Length_of_Pendulum, Period_of_Pendulum)
@@ -19,7 +20,3 @@ plt.yscale('log')
 plt.xscale('log')
 plt.title('Pendulum Length and Period for Simulation')
 plt.grid(True)
-
-#Real World Data
-#Length_of_Pendulum= [0.12065, 0.2159, 0.3429, 0.4699, 0.5969] 
-#Period_of_Pendulum= [0.6561, 0.8014,0.6091, 0.5376, 0.4753] #last data point made up
